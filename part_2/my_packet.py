e7eb4240bdaed88a8a283f449aeac3057caac209
None <None>
2018-11-29 02:43:02 -0800

    block send/recv 1th commit

--- part_2/my_packet.py
+++ part_2/my_packet.py
@@ -1,41 +1,68 @@
 from mininet.net import Mininet
+from mininet.topo import Topo
 from mininet.link import TCLink
 from mininet.clean import cleanup
+from mininet.cli import CLI
+from mininet.util import pmonitor
+from mininet.log import *
 import time
+
+
+class StarTopo(Topo):
+    "Simple topology example."
+
+    def __init__(self, computing_hosts_num=5):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__(self)
+
+        self.computing_hosts_num = computing_hosts_num
+        self.ch_list = []
+
+        # add center switch
+        s0 = self.addSwitch('s0', ip='10.0.0.255')
+        # add monitoring host
+        mh0 = self.addHost('mh0', ip='10.0.0.100')
+        # add computing hosts
+        for i in range(self.computing_hosts_num):
+            tmp_h = self.addHost('ch{}'.format(i + 1), ip='10.0.0.{}'.format(i + 1))
+            self.ch_list.append(tmp_h)
+        # add links
+        self.addLink(s0, mh0)
+        for tmp_h in self.ch_list:
+            self.addLink(s0, tmp_h)
 
 
 def test_packet_topo():
     # ----------- test start -------------
     cleanup()
-    net = Mininet(link=TCLink)
 
     # ----------- set topo -----------
-    # add center switch
-    s0 = net.addSwitch('s0', ip='10.0.0.255')
-    # add monitoring host
-    mh0 = net.addHost('mh0', ip='10.0.0.100')
-    # add computing hosts
-    computing_hosts_num = 10
-    ch_list = []
-    for i in range(computing_hosts_num):
-        tmp_h = net.addHost('ch{}'.format(i), ip='10.0.0.{}'.format(i))
-        ch_list.append(tmp_h)
-    # add links
-    net.addLink(s0, mh0)
-    for tmp_h in ch_list:
-        net.addLink(s0, tmp_h)
+    net = Mininet(topo=StarTopo(10), link=TCLink)
 
     # ----------- init net -------------
-    net.build()
     net.start()
     # first test
+    # CLI(net)
     net.pingAll()
-    s0.sendCmd('sudo python net_init/monitor_node_init.py &')
+
+    mh0 = net.get('mh0')
+    mh0.cmd('python net_init/monitor_node_init.py --self-ip {} &'.format(mh0.IP()))
+
+    tmp_h = net.get('ch1')
+    tmp_h.cmd('python net_init/computing_node_init.py --seed-ip {} &'.format(mh0.IP()))
+    print("----- start monitor ----- \n{}".format(mh0.IP()))
+    print(mh0.monitor())
+    print(tmp_h.monitor())
+
+    '''
+    mh0.sendCmd('sudo python net_init/monitor_node_init.py &')
     # use monitor host as hard-code seed in Bitcoin
     for tmp_h in ch_list:
         tmp_h.sendCmd('sudo python net_init/computing_node_init.py --ip {} &'.format(mh0.IP()))
         _tmp_result = tmp_h.waitOutput(verbose=False)
-
+    '''
     '''
     h1.sendCmd('python receive_packet.py --ip {} &'.format(h1.IP()))
     _tmp_result = h1.waitOutput()
@@ -49,12 +76,12 @@ def test_packet_topo():
 
     # make sure h1 got the final msg
     time.sleep(5)
-    for tmp_h in ch_list:
-        tmp_h.cmd('kill %python')
     mh0.cmd('kill %python')
+    tmp_h.cmd('kill %python')
     # ---------- test stop ------------
     net.stop()
 
 
 if __name__ == '__main__':
+    setLogLevel('info')
     test_packet_topo()
--- part_2/net_init/monitor_node_init.py
+++ part_2/net_init/monitor_node_init.py
@@ -1,23 +1,25 @@
 import socket
 import argparse
 
-s.sendto(args.data, (args.ip, args.port))
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='receive server help')
 
-    parser.add_argument('--self-ip', help='ip', default='127.0.0.1', type=str)
+    parser.add_argument('--self-ip', help='ip', default='localhost', type=str)
     parser.add_argument('--self-port', help='port', default=12345, type=int)
     args = parser.parse_args()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((args.self_ip, args.self_port))
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((args.self_ip, args.self_port))
+    server.listen(10)
 
     packet = open('data_backup/mh0_packet.txt', 'w')
 
     while True:
-        data, addr = s.recvfrom(512)
+        connect, addr = server.accept()
+        data = connect.recv(1024)
+        print(data)
         packet.write("%s: %s\n" % (addr, data))
         packet.flush()
-        data
+        connect.send('{}+my test.'.format(addr))
+    server.close()
